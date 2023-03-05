67c91544cf642d331b86ec1857490c5af09d5100
MmD <mmdj27634@gmail.com>
2023-03-05 15:59:48 +0330

    change : separate folders for client and server

--- src/client/utils/functions.ts
+++
@@ -1,3 +0,0 @@
-export const isLatin = (text : string) => {
-    return text?.split("").filter((item : string) => /[ء-ي]/.test(item)).length < text?.split("").filter((item : string) => /[a-zA-Z]/.test(item)).length
-}
